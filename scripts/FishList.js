1a7114e8635f7653251be00d3745a4f5b91bb822
Jisie DM <jisie.david@gmail.com>
2020-10-07 11:25:32 -0500

    Created FishList.js that appends header and container for holding list of fish to DOM

---
+++ scripts/FishList.js
@@ -0,0 +1,22 @@
+/**
+ *  FishList which renders individual fish objects as HTML
+ */
+
+// TODO: Import `useFish` from the data provider module
+
+export const FishList = () => {
+  const contentElement = document.querySelector(".contentContainer__left")
+
+  // debugger
+
+  console.log(contentElement)
+
+  contentElement.innerHTML += `
+  <section class="fishList">
+  <h3>Fish List</h3>
+  <div class="fishContainer">
+  Placeholder for list of fishes
+  </div>
+  </section>
+`
+}
